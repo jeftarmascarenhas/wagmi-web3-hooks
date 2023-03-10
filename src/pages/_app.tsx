5091be56d7d5f8495937ae625e8a1b34521fb667
jeff mascarenhas <>
2023-03-10 20:48:40 -0300

    feat: adding wagmi provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,44 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+import { WagmiConfig, createClient, configureChains } from "wagmi";
+import { avalanche, goerli, mainnet, optimism } from "wagmi/chains";
+
+// import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from "wagmi/providers/public";
+
+import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [mainnet, goerli, optimism, avalanche],
+  [publicProvider()]
+);
+
+const client = createClient({
+  autoConnect: true,
+  connectors: [
+    new MetaMaskConnector({
+      chains,
+      options: {
+        UNSTABLE_shimOnConnectSelectAccount: true,
+      },
+    }),
+    new CoinbaseWalletConnector({
+      chains,
+      options: {
+        appName: "Bush Associates",
+      },
+    }),
+  ],
+  provider,
+  webSocketProvider,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig client={client}>
+      <Component {...pageProps} />
+    </WagmiConfig>
+  );
 }
