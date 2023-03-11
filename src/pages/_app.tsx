6b9be0cecf55a0f68fe2ba0d384eb777289c1be8
Jeff Mascarenhas <jeftarmascarenhas@hotmail.com>
2023-03-10 21:57:20 -0300

    feat: youtube

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,44 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
-import { WagmiConfig, createClient, configureChains } from "wagmi";
-import { avalanche, goerli, mainnet, optimism } from "wagmi/chains";
-
-// import { alchemyProvider } from 'wagmi/providers/alchemy';
-import { publicProvider } from "wagmi/providers/public";
-
-import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
-import { MetaMaskConnector } from "wagmi/connectors/metaMask";
-
-const { chains, provider, webSocketProvider } = configureChains(
-  [mainnet, goerli, optimism, avalanche],
-  [publicProvider()]
-);
-
-const client = createClient({
-  autoConnect: true,
-  connectors: [
-    new MetaMaskConnector({
-      chains,
-      options: {
-        UNSTABLE_shimOnConnectSelectAccount: true,
-      },
-    }),
-    new CoinbaseWalletConnector({
-      chains,
-      options: {
-        appName: "Bush Associates",
-      },
-    }),
-  ],
-  provider,
-  webSocketProvider,
-});
-
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <WagmiConfig client={client}>
-      <Component {...pageProps} />
-    </WagmiConfig>
-  );
+  return <Component {...pageProps} />;
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,7 @@
-import Account from "./components/Account";
-import Connect from "./components/Connect";
-
 export default function Home() {
   return (
     <div>
-      <h1>Wagmi - Hooks Web3 for React</h1>
-      <Connect />
-      <br />
-      <Account />
+      <h1>NFT Choose | Wagmi - Hooks Web3 for React</h1>
     </div>
   );
 }
